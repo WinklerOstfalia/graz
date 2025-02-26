c609cf711a0dc93b03b5565329af640877a5322f
WinklerOstfalia <mor.winkler@ostfalia.de>
2025-02-26 12:57:44 +0100

    CSS hinzugefügt

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,13 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Graz Geschichten</title>
+    <?= css("assets/css/default.css") ?>
+</head>
+<body>
+</html><h1><?= $page->title() ?></h1>
+
+    
+</body>
