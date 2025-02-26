f1e570c21ba5378955db4a965d67433957b53378
WinklerOstfalia <mor.winkler@ostfalia.de>
2025-02-26 14:11:49 +0100

    beispielcontent upload

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,11 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Graz Geschichten</title>
+    <title><?= $site->title() ?></title>
     <?= css("assets/css/default.css") ?>
 </head>
 <body>
-</html><h1><?= $page->title() ?></h1>
+</html>
 
+    <h1><?= $page->title() ?></h1>
+    
+    <div>
+    <?php foreach($page->images() as $image): ?>
+    <figure>
+        <a href="<?= $image->url() ?>">
+            <img src="<?= $image->url() ?>" alt="">
+        </a>
+    </figure>
+        <?php endforeach ?>
+    </div>
+
+    <p><?= $page->intro() ?></p>
     
 </body>
